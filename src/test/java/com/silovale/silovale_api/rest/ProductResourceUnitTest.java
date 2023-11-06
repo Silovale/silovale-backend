d14069ac6d603c64381050c4fed3f1d7b4a8ebb4
DianCarolinee <Dianacs78@hotmail.com>
2023-11-06 11:55:08 -0500

    New port 3000 in yml file

--- src/test/java/com/silovale/silovale_api/rest/ProductResourceUnitTest.java
+++ src/test/java/com/silovale/silovale_api/rest/ProductResourceUnitTest.java
@@ -1,2 +1,55 @@
+package com.silovale.silovale_api.rest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.silovale.silovale_api.domain.Product;
+import com.silovale.silovale_api.model.ProductDTO;
+import com.silovale.silovale_api.repos.ProductRepository;
 
 
+@ExtendWith(MockitoExtension.class)
+public class ProductResourceUnitTest {
+
+    @InjectMocks
+    ProductResource productResource;
+
+    @Mock
+    ProductRepository productRepository;
+
+    @Test
+public void testAddProduct(){
+
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+    
+    Product product = new Product();
+    product.setId((long) 10000);
+    when(productRepository.save(any(Product.class))).thenReturn(product);
+    
+    ProductDTO productDTOToAdd = new ProductDTO();
+    productDTOToAdd.setCategory("Ropa");
+    productDTOToAdd.setDescription("Mi nuevo negocio");
+    productDTOToAdd.setImage("Base64");
+    productDTOToAdd.setPrice(120.00);
+    ResponseEntity<Long> responseEntity = productResource.createProduct(productDTOToAdd);
+
+ /*
+    assertThat(((Object) responseEntity).getCategory("Ropa")).isEqualTo(201);
+    assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/1");
+    */
+
+}
+                                     } 
